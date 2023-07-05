995fc57fd6d97ddf7364d352bfffb858bcedee3f
lundellm <maija.lundell@tuni.fi>
2023-07-05 12:07:31 +0300

    A person with certain id can be deleted. Tested with VS Code REST

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-app.get('/', (req, res) => {
+app.get('/api/persons/', (req, res) => {
     res.json(persons)
   })
   
@@ -38,21 +38,20 @@ app.get('/info/', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people <br><br> ${date} </p>`)
 })
 
-
-/*const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(persons))
-})
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
-  
-  app.get('/api/notes', (req, res) => {
-    res.json(notes)
-  })*/
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
