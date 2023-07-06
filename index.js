780a4aecf0c5947db81713a69d5395149f0b4f85
lundellm <maija.lundell@tuni.fi>
2023-07-06 11:38:48 +0300

    The program uses morgan to log actions to console with tiny configuration

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const morgan = require('morgan');
+
 const app = express()
 
+/*morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})*/
+
 app.use(express.json())
+
+app.use(morgan('tiny'));
 
 let persons = [
   {
