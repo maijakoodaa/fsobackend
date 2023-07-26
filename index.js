24885de0b258e5561a4938420343f839293d00d6
lundellm <maija.lundell@tuni.fi>
2023-07-26 11:36:05 +0300

    Deleting contacts is updated into DB

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(cors())
-
-app.use(express.json())
-
 app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
+
+
 
 morgan.token('data', request => {
   return JSON.stringify(request.body)
@@ -40,11 +49,6 @@ let persons = [
   }
 ]
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
 app.get('/api/persons/', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
@@ -53,25 +57,29 @@ app.get('/api/persons/', (request, response) => {
   
 app.get('/info/', (req, res) => {
     const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people <br><br> ${date} </p>`)
+    res.send(`<p>Phonebook has info for ${Person.length} people <br><br> ${date} </p>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
     if (person) {
       response.json(person)
     } else {
       response.status(404).end()
     }
   })
+  .catch(error => next(error))
+})
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      //const id = Number(request.params.id)
+      //persons = persons.filter(person => person.id !== id)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 function getRandomInt(max) {
@@ -111,4 +119,27 @@ app.post('/api/persons', (request, response) => {
   })
   //persons = persons.concat(person)
   //response.json(person)
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
